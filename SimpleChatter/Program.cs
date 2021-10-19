ae2e893bf56052bd4ff624b73ddf53b15ac524ba
Simon Kinch <simon.kinch@digitalleadership.edu.je>
2021-10-19 08:06:29 +0100

    SimpleChatter updated to respond to How is your day?

--- SimpleChatter/Program.cs
+++ SimpleChatter/Program.cs
@@ -17,21 +17,34 @@ namespace SimpleChatter
             Console.WriteLine("Hello " + usersName);
 
             // Ask the user a yes or no question such as “Are you having a good day?”
-            Console.WriteLine("Have you had a good day? Y or N");
+            Console.WriteLine("Have you had a good day?");
             string goodDay = Console.ReadLine();
+
+            // If they answer yes, display a response of your choosing
+            if (goodDay.Contains("No") == true)
+            {
+                Console.WriteLine("Sorry to hear that...");
+            }
+
+            else if (goodDay.Contains("no") == true)
+            {
+                Console.WriteLine("Sorry to hear that...");
+            }
+
+            else if (goodDay.Contains("n't") == true)
+            {
+                Console.WriteLine("Sorry to hear that...");
+            }
+
+            else
+            {
+                Console.WriteLine("Good to hear!");
+            }
+
+            // If they answer no, display an alternative response of your choosing
 
 
 /*
-
-
-
-
-
-Ask the user a yes or no question such as “Are you having a good day?”
-
-If they answer yes, display a response of your choosing
-
-If they answer no, display an alternative response of your choosing
 
 Update your program so it picks a random question from a set of at least 3, and so that it gives positive/negative answers that correspond to each question accordingly
 
