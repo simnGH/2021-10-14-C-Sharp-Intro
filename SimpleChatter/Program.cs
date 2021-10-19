ce6a2f2bff75a43cfb70110b42ff734ac8693e12
Simon Kinch <simon.kinch@digitalleadership.edu.je>
2021-10-19 19:17:58 +0100

    Three random question sets, as different methods

--- SimpleChatter/Program.cs
+++ SimpleChatter/Program.cs
@@ -5,7 +5,7 @@ namespace SimpleChatter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Question1()
         {
             // Add a line to the main method to print the text “Hello World!”
             Console.WriteLine("Hello World!");
@@ -14,24 +14,15 @@ namespace SimpleChatter
             Console.Out.WriteLine("What's your name?");
             string usersName = Console.ReadLine();
 
-            Console.WriteLine("Hello " + usersName);
+            Console.WriteLine($"Hello {usersName}");
 
             // Ask the user a yes or no question such as “Are you having a good day?”
             Console.WriteLine("Have you had a good day?");
             string goodDay = Console.ReadLine();
 
             // If they answer yes, display a response of your choosing
-            if (goodDay.Contains("No") == true)
-            {
-                Console.WriteLine("Sorry to hear that...");
-            }
-
-            else if (goodDay.Contains("no") == true)
-            {
-                Console.WriteLine("Sorry to hear that...");
-            }
-
-            else if (goodDay.Contains("n't") == true)
+            // If they answer no, display an alternative response of your choosing
+            if (goodDay.Contains("No") == true || goodDay.Contains("no") == true || goodDay.Contains("n't") == true)
             {
                 Console.WriteLine("Sorry to hear that...");
             }
@@ -41,14 +32,84 @@ namespace SimpleChatter
                 Console.WriteLine("Good to hear!");
             }
 
+        }
+
+        public static void Question2()
+        {
+            // Add a line to the main method to print the text “Hello World!”
+            Console.WriteLine("Hello Dude!");
+
+            // Ask the user for their name and change the message to “Hello, <name>!”
+            Console.Out.WriteLine("What's your favourite colour?");
+            string faveColour = Console.ReadLine();
+
+            Console.WriteLine($"{faveColour} is my favourite too!");
+
+            // Ask the user a yes or no question such as “Are you having a good day?”
+            Console.WriteLine("Do you smell gas?");
+            string goodDay = Console.ReadLine();
+
+            // If they answer yes, display a response of your choosing
             // If they answer no, display an alternative response of your choosing
+            if (goodDay.Contains("No") == true || goodDay.Contains("no") == true || goodDay.Contains("n't") == true)
+            {
+                Console.WriteLine("Might be my new aftershave...");
+            }
+
+            else
+            {
+                Console.WriteLine("Let's split...");
+            }
+        }
+
+        public static void Question3()
+        {
+            // Add a line to the main method to print the text “Hello World!”
+            Console.WriteLine("Hello Mate!");
+
+            // Ask the user for their name and change the message to “Hello, <name>!”
+            Console.Out.WriteLine("What are you drinking?");
+            string faveColour = Console.ReadLine();
+
+            Console.WriteLine($"A {faveColour}, and a scotch on the rocks for me.");
+
+            // Ask the user a yes or no question such as “Are you having a good day?”
+            Console.WriteLine("Are you driving?");
+            string goodDay = Console.ReadLine();
+
+            // If they answer yes, display a response of your choosing
+            // If they answer no, display an alternative response of your choosing
+            if (goodDay.Contains("No") == true || goodDay.Contains("no") == true || goodDay.Contains("n't") == true)
+            {
+                Console.WriteLine("Parrrrttaaaay!");
+            }
+
+            else
+            {
+                Console.WriteLine("You can drive us home, then. Barman, another scotch!");
+            }
+        }
 
 
-/*
 
-Update your program so it picks a random question from a set of at least 3, and so that it gives positive/negative answers that correspond to each question accordingly
+        static void Main(string[] args)
+        {
 
-*/
+            Random rnd = new Random();
+            int rndNumber = rnd.Next (1, 4);
+        
+            switch (rndNumber)
+            { 
+                case 1:
+                    Question1();
+                    break;
+                case 2:
+                    Question2();
+                    break;
+                default:
+                    Question3();
+                    break;
+            }
 
         }
     }
